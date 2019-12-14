e1f6c3561034ddc758405a46a3a7700e4b522983
Big-Bro222 <33903538+Big-Bro222@users.noreply.github.com>
2019-12-14 16:22:07 +0100

    creat enemyai

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyAI : MonoBehaviour
+{
+    
+    [SerializeField] Transform target;
+    [SerializeField] float detectablerange=5f;
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget=Mathf.Infinity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent= GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        distanceToTarget=Vector3.Distance(target.position,transform.position);
+        if(distanceToTarget<= detectablerange){
+             navMeshAgent.SetDestination(target.position);
+             
+        }
+        
+    }
+
+     void OnDrawGizmosSelected() {
+            Gizmos.color= new Color(1,1,0,0.75F);
+            Gizmos.DrawWireSphere(transform.position,detectablerange);
+        }
+    
+}
+
